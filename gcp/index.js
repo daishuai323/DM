02c69697f56fc8654906915ee38722186ea381c7
Boom McCloud <boom.mccloud@gmail.com>
2024-05-15 18:17:42 +0800

    changed port setting

--- gcp/index.js
+++ gcp/index.js
@@ -24,7 +24,7 @@ app.post('/publish', async (req, res) => {
   }
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
